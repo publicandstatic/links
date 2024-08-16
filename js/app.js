beafdeec3c1fa08e8c314a0f4c892bcb143f33f1
PUBLIC and STATIC <publicandstatic@gmail.com>
2024-08-16 04:03:58 +0300

    added logo animation

--- js/app.js
+++ js/app.js
@@ -19,10 +19,43 @@ document.getElementById('copy-email-btn').addEventListener('click', function(eve
     // Змінюємо текст посилання
     const link = document.getElementById('copy-email-btn');
     const originalText = link.innerHTML;
-    link.innerHTML = 'Email скопійовано';
+    link.innerHTML = 'Email скопійовано!';
 
     // Повертаємо оригінальний текст через 2 секунди
     setTimeout(function() {
         link.innerHTML = originalText;
     }, 1000);
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const logoElement = document.querySelector('.logo');
+    const logoChangeElements = document.querySelectorAll('.logo-change');
+    const images = [
+        '../images/logos/logo_crazy.png',
+        '../images/logos/logo_good.png',
+        '../images/logos/logo_hm.png',
+        '../images/logos/logo_nice.png',
+        '../images/logos/logo_ohmy.png',
+        '../images/logos/logo_ok.png',
+        '../images/logos/logo_oyy.png',
+        '../images/logos/logo_sad.png',
+        '../images/logos/logo_serios.png',
+        '../images/logos/logo_smile.png',
+        '../images/logos/logo_yea.png',
+        '../images/logos/logo_yyy.png',
+    ];
+
+    function getRandomImage() {
+        return images[Math.floor(Math.random() * images.length)];
+    }
+
+    logoChangeElements.forEach(element => {
+        element.addEventListener('mouseenter', () => {
+            logoElement.style.backgroundImage = `url('${getRandomImage()}')`;
+        });
+
+        element.addEventListener('mouseleave', () => {
+            logoElement.style.backgroundImage = `url('../images/logos/logo_main.png')`;
+        });
+    });
 });
